46f5a0bffc3dba7d2f1c1e945772e71b89fd6d31
MO-FA-EK <acer9012a@gmail.com>
2025-05-08 00:20:45 +0300

    g

--- project1/src/app/app.routes.ts
+++ project1/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from '../component/homepage/homepage.component';
 import { ServicesComponent } from '../component/services/services.component'; 
+import { AboutUsComponent } from '../component/about-us/about-us.component';
+import { LoginComponent } from '../component/login/login.component';
 export const routes: Routes = [
   {
     path: '',
@@ -17,5 +19,12 @@ export const routes: Routes = [
 
    path: 'services', component: ServicesComponent
 
+  },
+  {
+    path: 'about-us', component: AboutUsComponent
+  },
+  {
+    path: 'login', component: LoginComponent
   }
+
 ];
