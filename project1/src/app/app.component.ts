f55aef04cddeeffcb9962d63b5458dcd2221fdc4
MO-FA-EK <acer9012a@gmail.com>
2025-05-08 00:23:48 +0300

    2

--- project1/src/app/app.component.ts
+++ project1/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { HomepageComponent } from '../component/homepage/homepage.component';
 import { HeaderComponent } from '../component/header/header.component';
 import { ServicesComponent } from '../component/services/services.component';
 import { FooterComponent } from '../component/footer/footer.component';
+import { AboutUsComponent } from '../component/about-us/about-us.component';
+import { LoginComponent } from '../component/login/login.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    HomepageComponent,HeaderComponent,ServicesComponent,FooterComponent
+    HomepageComponent,HeaderComponent,ServicesComponent,FooterComponent,AboutUsComponent,LoginComponent
     
   ],
   templateUrl: './app.component.html',
